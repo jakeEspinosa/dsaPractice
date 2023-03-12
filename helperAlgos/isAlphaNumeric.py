5221ff177a7a972844135a19638a1cdb6424a294
Jake Espinosa <50299748+jakeEspinosa@users.noreply.github.com>
2023-03-12 16:00:41 -0700

    Update isAlphaNumeric.py

--- helperAlgos/isAlphaNumeric.py
+++ helperAlgos/isAlphaNumeric.py
@@ -1,6 +1,10 @@
-def isAlphaNumeric(char):
+'''
+Time Complexity: O(1)
 
+Space Complexity: O(1)
+'''
 
-while True:
-    userInput = input()
-    print(isAlphaNumeric(userInput))
+def isAlphaNum(char):
+    return (ord("A") <= ord(char) <= ord("Z") or
+            ord("a") <= ord(char) <= ord("z") or
+            ord("0") <= ord(char) <= ord("9"))
